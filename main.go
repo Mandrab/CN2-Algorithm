155e234d44ddcf74e09b8f8b8740c88d6ea9e077
Paolo Baldini <paolobaldini01@gmail.com>
2020-12-17 23:00:06 +0100

    added main structure of the upcoming algorithm

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Mandrab/CN2-Algorithm/rule/induction/algorithm"
+
+func main() {
+	// TODO management of input parameters
+	algorithm.CN2()
+}
---
+++ rule/induction/algorithm/cn2.go
@@ -0,0 +1,5 @@
+package algorithm
+
+func CN2() {
+	// TODO implementation of CN2 induction algorithm
+}
