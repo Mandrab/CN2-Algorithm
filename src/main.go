2bba44e5078bd11a8355c65aae3396a187ae6c81
Paolo Baldini <paolobaldini01@gmail.com>
2020-12-19 11:43:54 +0100

    first version of config parameters management

--- src/main.go
+++ src/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/Mandrab/CN2-Algorithm/src/src/rule/induction/algorithm"
+import (
+	"fmt"
+
+	"github.com/Mandrab/CN2-Algorithm/src/rule/induction/algorithm"
+)
 
 func main() {
 	// TODO management of input parameters
+	fmt.Println(algorithm.Parameters)
 	algorithm.CN2()
 }
---
+++ src/rule/induction/algorithm/parameters.go
@@ -0,0 +1,43 @@
+package algorithm
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type parameters struct {
+	Threshold   int    `yaml:"threshold"`
+	StarSetSize int    `yaml:"starSetSize"`
+	DatasetPath string `yaml:"datasetPath"`
+}
+
+// Parameters of the CN2 algorithm
+var Parameters parameters
+
+func init() {
+	// try to read config file. nothing wrong appens if it does not exists
+	if config, err := ioutil.ReadFile("cn2.config.yml"); err == nil {
+		// try to unmarshal yaml file and exit if some error is found
+		if err = yaml.Unmarshal(config, &Parameters); err != nil {
+			log.Fatalf("error: %v", err)
+		}
+	} else {
+		Parameters.Threshold = 90
+		Parameters.StarSetSize = 15
+		// TODO maybe default dataset?
+	}
+
+	// management of command line flags
+	threshold := flag.Int("t", Parameters.Threshold, "TODO usage description")
+	starSetSize := flag.Int("s", Parameters.StarSetSize, "TODO usage description")
+	datasetPath := flag.String("d", Parameters.DatasetPath, "TODO usage description")
+
+	flag.Parse()
+
+	Parameters.Threshold = *threshold
+	Parameters.StarSetSize = *starSetSize
+	Parameters.DatasetPath = *datasetPath
+}
