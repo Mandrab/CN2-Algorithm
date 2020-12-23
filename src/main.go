ce4149c92118ce36a2f2c3698d1d5a518b74806d
Paolo Baldini <paolobaldini01@gmail.com>
2020-12-23 08:20:40 +0100

    changes to parameter obtainment/setting structure

--- src/main.go
+++ src/main.go
@@ -6,22 +6,21 @@ import (
 	"log"
 	"os"
 
+	"github.com/Mandrab/CN2-Algorithm/src/config/cn2"
 	"github.com/Mandrab/CN2-Algorithm/src/rule/induction/algorithm"
 	"github.com/tobgu/qframe"
 )
 
 func main() {
-	// get algorithm parameters
-	parameters := algorithm.Parameters
 
-	// load dataset in CSV form
-	dsfile, err := os.Open(parameters.DatasetPath)
+	// open file in CSV form and load it as dataset
+	dsfile, err := os.Open(cn2.DatasetPath())
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
 	ds := qframe.ReadCSV(bufio.NewReader(dsfile))
 
 	// start CN2 algorithm with specified parameters and dataset
-	rules := algorithm.CN2(parameters, ds)
+	rules := algorithm.CN2(cn2.Threshold(), cn2.StarSetSize(), ds)
 	fmt.Println(rules)
 }
