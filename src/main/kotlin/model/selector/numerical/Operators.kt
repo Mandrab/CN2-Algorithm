85fac2e5b817bb00b1c06109c288078460b1503c
Paolo Baldini <paolobaldini01@gmail.com>
2021-01-14 14:52:16 +0100

    resolved bug in numerical operator test

--- src/main/kotlin/model/selector/numerical/Operators.kt
+++ src/main/kotlin/model/selector/numerical/Operators.kt
@@ -1,10 +1,12 @@
 package model.selector.numerical
 
+import kotlin.math.abs
+
 const val Delta = 0.00001
 
 enum class Operators(val test: (selector: Numerical, value: Double) -> Boolean) {
-    Equal(test = { s, v -> kotlin.math.abs(s.value - v) < Delta }),
-    Different(test = { s, v -> kotlin.math.abs(s.value - v) > Delta }),
-    Greater(test = { s, v -> s.value - v > Delta }),
-    LesserEqual(test = { s, v -> s.value - v <= Delta })
+    Equal(test = { s, v -> abs(s.value - v) < Delta }),
+    Different(test = { s, v -> abs(s.value - v) > Delta }),
+    Greater(test = { s, v -> s.value - Delta < v }),
+    LesserEqual(test = { s, v -> s.value + Delta >= v })
 }
